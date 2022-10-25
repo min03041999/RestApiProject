6efc6d64eedde60a2292ba81b6be5eba8270736a
Min Kookie <min03041999@gmail.com>
2022-10-25 10:24:51 +0700

    Transforming Then Catch to Async Await

--- controllers/feed.js
+++ controllers/feed.js
@@ -21,38 +21,42 @@ exports.getPosts = (req, res, next) => {
     });
 };
 
-exports.getPostsPage = (req, res, next) => {
+exports.getPostsPage = async (req, res, next) => {
   //Show by page
   const currentPage = req.query.page || 1;
   const perPage = 3;
-  let totalItems;
-  Post.find()
-    .countDocuments()
-    .then((count) => {
-      totalItems = count;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then((posts) => {
-      res.status(200).json({
-        message: "Fetched posts successfully.",
-        posts: posts,
-        totalItems: totalItems,
-      });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+
+    res.status(200).json({
+      message: "Fetched posts successfully.",
+      posts: posts,
+      totalItems: totalItems,
     });
-  // Show all
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+  // let totalItems;
   // Post.find()
+  //   .countDocuments()
+  //   .then((count) => {
+  //     totalItems = count;
+  //     return Post.find()
+  //       .skip((currentPage - 1) * perPage)
+  //       .limit(perPage);
+  //   })
   //   .then((posts) => {
-  //     res
-  //       .status(200)
-  //       .json({ message: "Fetched posts successfully.", posts: posts });
+  //     res.status(200).json({
+  //       message: "Fetched posts successfully.",
+  //       posts: posts,
+  //       totalItems: totalItems,
+  //     });
   //   })
   //   .catch((err) => {
   //     if (!err.statusCode) {
