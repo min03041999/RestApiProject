459d8e8a5c496a5400d91644626c131231aaa092
Min Kookie <min03041999@gmail.com>
2022-11-05 11:09:19 +0700

    Version N1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 const authRoutes = require("./routes/auth");
-
+const port = process.env.PORT || 3000;
 const app = express();
 // const cors = require("cors");
 const { v4: uuid } = require("uuid");
@@ -72,7 +72,7 @@ mongoose
     // app.listen(8080);
     // console.log("Server is running on PORT 8080");
 
-    const server = app.listen(8000);
+    const server = app.listen(port);
     console.log("Server is running on PORT 8080");
     const io = require("./socket").init(server);
     io.on("connection", (socket) => {
